ca17564a73afad8c8a05aabf24fc70119df6d171
Taiseer Ashouri <118752825+taiseer999@users.noreply.github.com>
2024-05-20 09:25:41 +0300

    Update uservar.py

--- repo/plugin.program.funs19wiz/uservar.py
+++ repo/plugin.program.funs19wiz/uservar.py
@@ -4,7 +4,7 @@ import xbmcaddon
 buildfile = 'https://taiseer999.github.io/Builds.txt'
 
 '''#####-----Notifications File-----#####'''
-notify_url  = ''
+notify_url  = 'https://github.com/taiseer999/taiseer999.github.io/blob/main/Notify.txt
 
 '''#####-----Excludes-----#####'''
 excludes  = ['plugin.video.whatever']
