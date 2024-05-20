bc2a49089396662ecb6d805d3ccb1c59335868d2
Taiseer Ashouri <118752825+taiseer999@users.noreply.github.com>
2024-05-20 09:22:00 +0300

    test

---
+++ repo/plugin.program.funs19wiz/uservar.py
@@ -0,0 +1,10 @@
+import xbmcaddon
+
+'''#####-----Build File-----#####'''
+buildfile = 'https://taiseer999.github.io/Builds.txt'
+
+'''#####-----Notifications File-----#####'''
+notify_url  = ''
+
+'''#####-----Excludes-----#####'''
+excludes  = ['plugin.video.whatever']
