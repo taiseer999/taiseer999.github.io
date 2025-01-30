d3bce6823729e495237730782139985b0c07e82c
Taiseer Ashouri <118752825+taiseer999@users.noreply.github.com>
2025-01-30 10:00:54 +0300

    clean

--- repo/skin.estuary.modv2cpm/scripts/context_menu_moviessets.py
+++
@@ -1,9 +0,0 @@
-import xbmc
-import xbmcgui
-
-home = xbmcgui.Window(10000)
-skin = home.getProperty("CurrentSkin")
-if skin == "skin.estuary.modv2cpm":
-    xbmc.executebuiltin("ActivateWindow(1132)")
-else:
-    pass 
