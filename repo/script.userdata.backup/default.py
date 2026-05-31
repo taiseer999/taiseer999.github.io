6099e0239a39cc59698a2a4b8bf9c4eb3245577c
Abukarim <118752825+taiseer999@users.noreply.github.com>
2026-05-31 10:50:08 +0300

    clean

--- repo/script.userdata.backup/default.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'resources', 'lib'))
-
-from backup_manager import BackupManager
-
-if __name__ == '__main__':
-    manager = BackupManager()
-    manager.run()
