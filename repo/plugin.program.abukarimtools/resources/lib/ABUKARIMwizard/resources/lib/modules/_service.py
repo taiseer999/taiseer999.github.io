4a0a339c4e1026b5c7b00a665a9fdab78f8c403f
Abukarim <118752825+taiseer999@users.noreply.github.com>
2026-06-06 10:00:24 +0300

    tst

--- repo/plugin.program.abukarimtools/resources/lib/ABUKARIMwizard/resources/lib/modules/_service.py
+++ repo/plugin.program.abukarimtools/resources/lib/ABUKARIMwizard/resources/lib/modules/_service.py
@@ -171,10 +171,13 @@ class Startup:
             backup_gui_skin(gui_save_default)
             setting_set('firstrun', 'false')
             # --- فتح Skin Installer تلقائياً عند أول تشغيل بعد تثبيت البيلد ---
-            xbmc.sleep(2000)
-            xbmc.executebuiltin(
-                'RunPlugin(plugin://plugin.program.abukarimtools/?mode=skin_install)'
-            )
+            # ننتظر حتى تكتمل الواجهة قبل فتح النافذة لتجنب خطأ Control 100
+            monitor = xbmc.Monitor()
+            monitor.waitForAbort(10)
+            if not monitor.abortRequested():
+                xbmc.executebuiltin(
+                    'RunPlugin(plugin://plugin.program.abukarimtools/?mode=skin_install)'
+                )
         else:
             if setting('autoclearpackages') == 'true':
                 clear_packages_startup()
