c0ba9de242f50c59b56274d0825366a19a44419a
Abukarim <118752825+taiseer999@users.noreply.github.com>
2026-08-04 19:39:27 +0300

    fx

--- repo/zips/plugin.program.abukarimtools/plugin.program.abukarimtools/resources/lib/dplex_toggle.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""DPlex tab on/off – thin wrapper around tab_toggle."""
-from resources.lib import tab_toggle
-
-
-def run():
-    tab_toggle.run(key='dplex', slot='1104', label='DPlex')
--- repo/zips/plugin.program.abukarimtools/plugin.program.abukarimtools/resources/lib/korean_toggle.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""Korean Media tab on/off – thin wrapper around tab_toggle."""
-from resources.lib import tab_toggle
-
-
-def run():
-    tab_toggle.run(key='korean', slot='1103', label='Korean Media')
